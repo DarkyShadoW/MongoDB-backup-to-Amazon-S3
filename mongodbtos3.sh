5a76ff11b85be1176d7b892180e2c9235eca4c1a
Marcos Muñoz Nuñez <marcosmunoz@playmuser.com>
2017-02-09 20:03:18 +0100

    Adapted for MongoDB Server buckups

--- mongodbtos3.sh
+++ mongodbtos3.sh
@@ -1,18 +1,20 @@
+
 #!/bin/sh
 
-# Updates etc at: https://github.com/woxxy/MySQL-backup-to-Amazon-S3
+# Original one for MYSQL at: https://github.com/woxxy/MySQL-backup-to-Amazon-S3
+#This varition can be found at: https://github.com/DarkyShadoW/MongoDB-backup-to-Amazon-S3
+
 # Under a MIT license
 
+#REQUIRES mongodump 3.2 or above to use the --gzip option
+
 # change these variables to what you need
-MYSQLROOT=root
-MYSQLPASS=password
-S3BUCKET=bucketname
+MONGOROOT=root
+MONGOPASS=password
+S3BUCKET=s3bucketname
 FILENAME=filename
-DATABASE='--all-databases'
 # the following line prefixes the backups with the defined directory. it must be blank or end with a /
-S3PATH=mysql_backup/
-# when running via cron, the PATHs MIGHT be different. If you have a custom/manual MYSQL install, you should set this manually like MYSQLDUMPPATH=/usr/local/mysql/bin/
-MYSQLDUMPPATH=
+S3PATH=mongodev_backup/
 #tmp path.
 TMP_PATH=~/
 
@@ -35,15 +37,8 @@ echo "Selected period: $PERIOD."
 
 echo "Starting backing up the database to a file..."
 
-# dump all databases
-${MYSQLDUMPPATH}mysqldump --quick --user=${MYSQLROOT} --password=${MYSQLPASS} ${DATABASE} > ${TMP_PATH}${FILENAME}.sql
 
-echo "Done backing up the database to a file."
-echo "Starting compression..."
-
-tar czf ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz ${TMP_PATH}${FILENAME}.sql
-
-echo "Done compressing the backup file."
+mongodump --gzip  --port 27017 -u ${MONGOROOT} -p ${MONGOPASS} --authenticationDatabase admin --archive=${TMP_PATH}${FILENAME}${DATESTAMP}.gz
 
 # we want at least two backups, two months, two weeks, and two days
 echo "Removing old backup (2 ${PERIOD}s ago)..."
@@ -56,12 +51,11 @@ echo "Past backup moved."
 
 # upload all databases
 echo "Uploading the new backup..."
-s3cmd put -f ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
+s3cmd put -f ${TMP_PATH}${FILENAME}${DATESTAMP}.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
 echo "New backup uploaded."
 
 echo "Removing the cache files..."
 # remove databases dump
-rm ${TMP_PATH}${FILENAME}.sql
-rm ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz
+rm ${TMP_PATH}${FILENAME}${DATESTAMP}.gz
 echo "Files removed."
 echo "All done."
